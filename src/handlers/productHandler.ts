96081491cfdad64f30444175d3915fd6e60460ca
Vadim <modstils88@mail.ru>
2025-02-26 15:04:48 +0300

    changed logyc

--- src/handlers/productHandler.ts
+++ src/handlers/productHandler.ts
@@ -1,18 +1,28 @@
-import TelegramBot from 'node-telegram-bot-api';
-import { products } from '../data/products';
-import { createMainKeyboard } from '../keyboards/keyboard';
-import { config } from '../config/config';
-
-
-
+import TelegramBot from "node-telegram-bot-api";
+import { createMainKeyboard } from "../keyboards/keyboard";
+import { config } from "../config/config";
+import { getProducts } from "../database";
 export const handleProducts = async (bot: TelegramBot, chatId: number) => {
-    try {
-        const productList = products.map(product => `🔸 ${product.name}`).join('\n');
-
-   await bot.sendMessage(chatId, `${config.messages.products}\n${productList}`, {
-        reply_markup: createMainKeyboard()
-    });
-    } catch (error) {
-        console.error('Ошибка при отправке сообщения:', error);
+  try {
+    const products = await getProducts();
+    for (const product of products) {
+      // Отправляем изображение
+      if (product.image_path) {
+        await bot.sendPhoto(chatId, product.image_path);
+      }
+      // Формируем текст с информацией о продукте
+      let productText = `🔸 ${product.name}\n`;
+      if (product.price) {
+        productText += `💵 Цена: ${product.price} руб.\n`;
+      }
+      if (product.description) {
+        productText += `📝 Описание: ${product.description}\n`;
+      }
+      await bot.sendMessage(chatId, productText, {
+        reply_markup: createMainKeyboard(),
+      });
     }
-}
+  } catch (error) {
+    console.error("Ошибка при отправке сообщения:", error);
+  }
+};
