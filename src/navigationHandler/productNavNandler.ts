ff2c98c6a31d6339b4d4482e7f02931d67441d70
Vadim <modstils88@mail.ru>
2025-03-24 16:21:34 +0300

    added new logics

--- src/navigationHandler/productNavNandler.ts
+++ src/navigationHandler/productNavNandler.ts
@@ -1,14 +1,21 @@
 import TelegramBot from "node-telegram-bot-api";
 import { handleProducts } from "../handlers/productHandler";
 
-
 export const productNavNandler = (bot: TelegramBot) => {
-    bot.onText(/\/product/, async (msg: TelegramBot.Message) => {
-        try {
-            
-            await handleProducts(bot, msg.chat.id);
-        } catch (error) {
-            console.error("Ошибка при обработке продуктов:", error);
-        }
-    });
+  bot.onText(/\/product/, async (msg: TelegramBot.Message) => {
+    try {
+      // Запрашиваем у пользователя текст для поиска
+      await bot.sendMessage(
+        msg.chat.id,
+        "Введите название продукта для поиска:",
+      );
+      // Ожидаем ответ от пользователя
+      bot.on("message", async (response: TelegramBot.Message) => {
+        const searchTerm = response.text || ""; // получаем текст введенный пользователем
+        await handleProducts(bot, msg.chat.id, searchTerm);
+      });
+    } catch (error) {
+      console.error("Ошибка при обработке продуктов:", error);
+    }
+  });
 };
