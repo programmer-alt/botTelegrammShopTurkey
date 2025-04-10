bf7a98af5d3c649afc4ca6db26be3359b0b97dba
Vadim <modstils88@mail.ru>
2025-04-11 00:29:12 +0300

    in client frontent+backend

---
+++ client/src/components/UploadComponent.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+// Основной компонент
+
+const UploadComponent = () => {
+  // Состояние для управления выбранным файлом, загрузкой и сообщением
+  const [file, setFile] = useState<File | null>(null);
+  const [upLoading, setIsUploading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  // Функция для обработки загрузки
+  const handleUpload = async () => {
+    // Проверка, выбран ли файл
+    if (!file) {
+      setMessage(" Выберете файл");
+      return;
+    }
+    // Установка состояния загрузки и очистка предыдущего сообщения
+    setIsUploading(true);
+    setMessage("");
+    try {
+      // Создание объекта FormData для отправки файлов
+      const formData = new FormData();
+      // Добавляем файл в объект FormData под именем 'file' для последующей отправки на сервер
+      formData.append("file", file);
+
+      // Отправка запроса на сервер
+      const response = await axios.post("api/products/upload", formData, {
+        // Устанавливаем заголовок 'Content-Type' как 'multipart/form-data', чтобы указать серверу, что данные отправляются в формате, подходящем для загрузки файлов
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      // Обработка успешной загрузки
+      setMessage(response.data.message || "Товары успешно загружены!");
+    } catch (error) {
+      setMessage(" Ошибка при загрузке файла");
+      console.error(" Ошибка загрузки", error);
+    } finally {
+      // Очистка состояния загрузки после завершения операции
+      setIsUploading(false);
+    }
+  };
+  return (
+    <div className='upload-container'>
+        <h2 className="upload-title" > Загрузка товаров</h2>
+        {/* Контейнер для ввода файла */}
+        <div className="upload-input-container">
+            <input 
+            type="file"
+            accept=".xlsx, .csv"
+            onChange={(e) => setFile(e.target.files?.[0] || null)}
+            disabled={upLoading}
+            className="upload-input"
+            />
+        </div>
+        {/* Кнопка загрузки */}
+        <button 
+      onClick={handleUpload}
+      disabled={upLoading}
+      className={`upload-button ${upLoading ? 'loading' : ''}`}
+    >
+      {upLoading ? 'Загрузка...' : 'Загрузить товары'}
+    </button>
+    <p className= {`upload-message ${message.includes(' Ошибка ') ? 'error' : 'success'}`}>{message}</p>
+    </div>
+  );
+};
+export default UploadComponent;
