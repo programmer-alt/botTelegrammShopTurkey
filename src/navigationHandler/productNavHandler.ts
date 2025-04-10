c4904adeb68eaf5bdee5544aad23c0a0109969e4
Vadim <modstils88@mail.ru>
2025-04-11 00:11:07 +0300

    updated functionality

--- src/navigationHandler/productNavHandler.ts
+++ src/navigationHandler/productNavHandler.ts
@@ -2,7 +2,7 @@ import TelegramBot from "node-telegram-bot-api";
 import { productHandler } from "../handlers/productHandler";
 
 
-const userState = new Map();
+
 export const productNavHandler = (bot: TelegramBot) => {
   bot.onText(/\/product/, async (msg: TelegramBot.Message) => {
     const chatId = msg.chat.id;
