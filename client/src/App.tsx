a847d8f40f57af52abc5b94035bcc47f727c6c1f
Vadim <modstils88@mail.ru>
2025-05-01 20:52:31 +0300

    feat(client): add ProductList component to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import UploadComponent from './components/UploadComponent';
 import './UploadComponent.css';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import ProductList from 'services/ProductList';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="app-container">
-      <UploadComponent/>  
+      <UploadComponent/>
+      <ProductList /> 
     </div>
+    </Provider>
   );
 }
 
