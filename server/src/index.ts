fed0a69637b6ef4148f4894bae17d58ab9fff064
Vadim <modstils88@mail.ru>
2025-05-25 11:46:06 +0300

    Update .gitignore, add tsconfig, enhance parsers, and fix productHandler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import productsRouter from './routes/products';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,8 @@ app.use(express.json());
 
 // GET /api/products — получение списка продуктов для клиента
 app.use('/api/products', productsRouter);
+// отдача статики из папки imagesProducts
+app.use('/imagesProducts', express.static(path.join(__dirname, '../../src/imagesProducts')));
 
 
 app.listen(PORT, () => {
