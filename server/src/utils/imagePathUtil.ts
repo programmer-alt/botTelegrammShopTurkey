9f177c78ed47cfda76926ff6c8935694491cc282
Vadim <modstils88@mail.ru>
2025-05-25 18:50:43 +0300

    Add newline and optimize getImagePath function

--- server/src/utils/imagePathUtil.ts
+++ server/src/utils/imagePathUtil.ts
@@ -4,6 +4,7 @@ export const getImagePath = (imagePath: string | undefined): string | undefined 
     if (!imagePath) {
         return undefined;
     }
+   
     const fileName = path.basename(imagePath);
     return path.join(__dirname, '../../src/imagesProducts', fileName );
 };
